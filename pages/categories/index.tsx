93ded08f3d4a8ff721c588b9cc7a6cae8a86430f
Jesse Gerard Brands <me@jgbrands.nl>
2021-01-02 17:52:40 +0100

    GridView skeleton.

---
+++ pages/categories/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GridView from "../../components/ui/grid-view";
+
+export default function() {
+    return (
+        <React.Fragment>
+            <GridView />
+        </React.Fragment>
+    )
+}
